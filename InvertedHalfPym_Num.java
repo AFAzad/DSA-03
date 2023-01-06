08ab38247f58530db5e65307d5357fe269569af8
Abul Fajle Azad <abulfajle405@gmail.com>
2023-01-06 23:51:07 +0530

    Initial Commit

---
+++ InvertedHalfPym_Num.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+// Q. no.1 Print the Pattern: Inverted Half Pyramid with number.
+public class InvertedHalfPym_Num {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 1; i<= n; i++){
+            
+            // innerr Loop
+            for(int j =1; j<=n-i+1; j++){
+                System.out.print(j);
+            } 
+            System.out.println();
+        }
+     
+        
+
+
+    // Q no. 2 : Print the pa    
+    }
+}
